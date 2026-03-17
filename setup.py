806fbace36ca534721f7e71cd437a9eeb92e18aa
Naveen <naveenyadavgolla04@gmail.com>
2026-03-17 03:02:49 -0500

    requirements Done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="wealthGPT",
+    version="0.1.0",
+    author="Naveen Yadav Golla",
+    author_email="naveenyadavgollao4@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
